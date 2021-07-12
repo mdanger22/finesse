c400c0e23323607a8847f7ccd792ebe1be436923
thedang <thedang@MacBook-Pro-2.local>
2021-07-12 02:56:24 -0700

    finished part three

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 
-const AboutPage = () => {
+const About = () => {
   return (
     <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
@@ -9,4 +9,4 @@ const AboutPage = () => {
   )
 }
 
-export default AboutPage
+export default About
